9d61d7215532710033d5e01035c7c482212d4ec6
ashishdangwal <ashish151186@gmail.com>
2020-08-12 22:04:49 -0500

    Fatory Design Pattern

---
+++ FactoryDesignPattern/src/SMSNotification.java
@@ -0,0 +1,8 @@
+public class SMSNotification implements Notification {
+    @Override
+    public void notifyUser()
+    {
+        // TODO Auto-generated method stub
+        System.out.println("Sending an SMS notification");
+    }
+}
