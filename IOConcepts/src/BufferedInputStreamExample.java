bfe711651a901a2bf6e2bb8d78f062604b615417
ashishdangwal <ashish151186@gmail.com>
2020-08-18 02:54:20 -0500

    New files for IO practice and nested classes

---
+++ IOConcepts/src/BufferedInputStreamExample.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class BufferedInputStreamExample{
+    int abc = 10;
+    public static void main(String args[]){
+        try{
+            FileInputStream fin=new FileInputStream("C:\\JavaPractice\\testout.txt");
+            BufferedInputStream bin=new BufferedInputStream(fin);
+            int i;
+            while((i=bin.read())!=-1){
+                System.out.print((char)i);
+            }
+            bin.close();
+            fin.close();
+        }catch(Exception e){System.out.println(e);}
+    }
+}
